b6aa5fbc0d412a990f218acc73344ac183904c11
Anurag A S <sampathanurag3@gmail.com>
2019-10-06 11:48:31 +0530

    Added Multiple colour and colour randomization

--- colouring_board.py
+++ colouring_board.py
@@ -2,8 +2,27 @@ from utils.testing_board import *
 from utils.board_size_choose import *
 from utils.pic_generator import *
 import pymsgbox
+import random
 
-colours = {'peach': [255, 251, 143], 'olive': [40, 250, 106]}
+"""
+Colour list
+Olive - [40, 250, 106], Light Blue - [66, 140, 252], Dark Blue - [0, 13, 255]
+Purple - [140, 0, 255], Baby Pink - [255, 92, 247], Light Red - [255, 92, 92]
+Red - [255, 0, 0], Grey - [97, 85, 85], Dark Green - [70, 77, 2]
+Green Blue - [0, 255, 179]
+
+"""
+colours = [[40, 250, 106], [66, 140, 252], [0, 13, 255], [140, 0, 255], [255, 92, 247], [255, 92, 92],
+           [255, 0, 0], [70, 77, 2], [0, 255, 179]]
+default_colours = {'peach': [255, 251, 143]}
+
+
+def randomize_colour():
+    """
+    Randomize the forward colour in the image
+    :return: Index of the colour
+    """
+    return random.randint(0, len(colours)-1)
 
 
 def colour_board(image_data, block_coords, board, interval_size):
@@ -15,20 +34,21 @@ def colour_board(image_data, block_coords, board, interval_size):
     :param interval_size: Interval for colour changing
     :return: Coloured image
     """
+    random_colour = randomize_colour()
     for i in range(board):
         for j in range(board):
-            image_data[i][j] = colours['peach']
+            image_data[i][j] = default_colours['peach']
     for block in block_coords:
         for x in range(block[0]*interval_size, (block[0]*interval_size)+interval_size):
             for y in range(block[1]*interval_size, (block[1]*interval_size)+interval_size):
-                image_data[x][y] = colours['olive']
+                image_data[x][y] = colours[random_colour]
     return image_data
 
 
 if __name__ == "__main__":
     while True:
         try:
-            input_size = int(pymsgbox.prompt('Image size?', default='8'))
+            input_size = int(pymsgbox.prompt('Image size?', default='5'))
             break
         except ValueError:
             pymsgbox.alert("Error, please input a valid size")
